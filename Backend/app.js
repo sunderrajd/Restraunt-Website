fc3756859344f19e526f709d4e7e2e05b24069b4
D Sunder Raj <dsunderraj37@gmail.com>
2025-11-28 02:16:19 +0530

    prepare backend for deploy

--- Backend/app.js
+++ Backend/app.js
@@ -10,8 +10,8 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["POST"],
+    origin: [process.env.FRONTEND_URL, "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
